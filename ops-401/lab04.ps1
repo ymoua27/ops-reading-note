c1ecc5f60aeca2ae4d92b4459096b2b86107db96
Yue Moua <147285396+ymoua27@users.noreply.github.com>
2024-01-11 18:56:39 -0800

    Added code

--- ops-401/lab04.ps1
+++ ops-401/lab04.ps1
@@ -20,3 +20,22 @@ if ($currentMinimumLength -ge $minimumPasswordLength) {
 
     Write-Host "Minimum password length has been set to $minimumPasswordLength or more."
 }
+
+
+# Set the desired SMB v1 client driver configuration
+$smbV1ClientDriverConfig = 1  # 'Enabled: Disable driver'
+
+# Registry path for SMB v1 client driver configuration
+$regPath = "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters"
+
+# Check if the current SMB v1 client driver configuration is already set
+$currentSmbV1ClientDriverConfig = (Get-ItemProperty -Path $regPath -Name "SMB1").SMB1
+
+if ($currentSmbV1ClientDriverConfig -eq $smbV1ClientDriverConfig) {
+    Write-Host "SMB v1 client driver configuration is already set to 'Enabled: Disable driver'."
+} else {
+    # Set the SMB v1 client driver configuration to the desired value
+    Set-ItemProperty -Path $regPath -Name "SMB1" -Value $smbV1ClientDriverConfig
+
+    Write-Host "SMB v1 client driver configuration has been set to 'Enabled: Disable driver'."
+}
