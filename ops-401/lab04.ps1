87c4734d39c67068c6dc55f488c915c71d53a00d
Yue Moua <147285396+ymoua27@users.noreply.github.com>
2024-01-11 18:53:02 -0800

    Create lab04.ps1

---
+++ ops-401/lab04.ps1
@@ -0,0 +1,17 @@
+# Set the desired minimum password length
+$minimumPasswordLength = 14
+
+# Registry path for minimum password length
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+
+# Check if the current minimum password length is already set
+$currentMinimumLength = (Get-ItemProperty -Path $regPath).MinimumPasswordLength
+
+if ($currentMinimumLength -ge $minimumPasswordLength) {
+    Write-Host "Minimum password length is already set to $minimumPasswordLength or more."
+} else {
+    # Set the minimum password length to the desired value
+    Set-ItemProperty -Path $regPath -Name "MinimumPasswordLength" -Value $minimumPasswordLength
+
+    Write-Host "Minimum password length has been set to $minimumPasswordLength or more."
+}
